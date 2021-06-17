f4aaa9683569d79525a35cf8ebb2d0b46ef7ada7
Maximilian Schulze <maximilian.schulze@student.hpi.uni-potsdam.de>
2021-06-17 14:27:12 +0200

    begin peak extraction

--- src/main/kotlin/heartk/eda/EdaPhasicTonicFeatures.kt
+++ src/main/kotlin/heartk/eda/EdaPhasicTonicFeatures.kt
@@ -1,6 +1,7 @@
 package heartk.eda
 
 import uk.me.berndporr.iirj.Butterworth
+import kotlin.math.sign
 
 object EdaPhasicTonicFeatures {
     fun getFeatures(
@@ -43,5 +44,20 @@ object EdaPhasicTonicFeatures {
         Butterworth()
 
         return features
+    }
+
+    /**
+     * Returns the indices where local maxima are.
+     *
+     * @param signal
+     * @return
+     */
+    fun findPeaks(signal: DoubleArray): DoubleArray {
+        val startIndex = 1 // First index cannot be maximum
+        val stopIndex = signal.size - 1 // Last index cannot be maximum
+        val peaks = mutableListOf<Int>()
+        val peakStart = mutableListOf<Int>()
+        val peakEnd = mutableListOf<Int>()
+
     }
 }
